6b74626b61ea84af2e5e04e41314ee47d33b8dbd
omarosama50ga-boop <omarosama50ga@gmail.com>
2025-10-05 20:51:54 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
      // متغيرات التطبيق
-     
-  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2026869167517898"
-     crossorigin="anonymous"></script>
+
         let debts = JSON.parse(localStorage.getItem('advertisements')) || [];
         let currentDebtId = null;
         let filteredDebts = [...debts];
